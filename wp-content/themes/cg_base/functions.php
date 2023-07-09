827ae8ff5596ec9753993d67788059ba7d446575
Chrisgarlick <cgarlick94@gmail.com>
2023-07-09 15:52:23 +0100

    jquery, sass and tailwind all sorted and working

--- wp-content/themes/cg_base/functions.php
+++ wp-content/themes/cg_base/functions.php
@@ -10,11 +10,18 @@ add_action('after_setup_theme', 'my_theme_register_timber');
 
 
 function enqueue_theme_scripts() {
-    wp_enqueue_script( 'cg_base-script', get_template_directory_uri() . '/assets/dist/js/scripts.min.js', array(), '1.0.0', true );
-    wp_enqueue_style( 'cd_base-style', get_template_directory_uri() . '/assets/dist/css/styles.min.css', array(), '1.0.0' );
+    // Deregister the default version of jQuery provided by WordPress
+    wp_deregister_script('jquery');
+
+    // Register and enqueue jQuery from an external source
+    wp_register_script('jquery', "//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js", array(), null, true);
+    wp_enqueue_script('jquery');
+
+    wp_register_script('fontawesome', "https://kit.fontawesome.com/ca004fe30a.js", array(), null, true);
+    wp_enqueue_script('fontawesome');
+
+    wp_enqueue_script( 'cg_base-script', get_template_directory_uri() . '/assets/dist/js/scripts.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_style( 'cg_base-style', get_template_directory_uri() . '/assets/dist/css/styles.min.css', array(), '1.0.0' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_theme_scripts' );
 
-// function enqueue_theme_styles() {
-// }
-// add_action( 'wp_enqueue_styles', 'enqueue_theme_styles' );
